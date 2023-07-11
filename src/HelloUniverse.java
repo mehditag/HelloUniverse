eb693d89c66cb1718355b11d7e6a18250ce3b195
mehditag <mehdiitaguema@gmail.com>
2023-07-11 13:53:14 +0200

    camelCase corrections

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -4,61 +4,61 @@ public class HelloUniverse {
 
     public static void main(String... args) {
 
-        Planete Mercure= new Planete();
-        Mercure.nom = "Mercure";
-        Mercure.diametre=4880;
-        Mercure.matiere="Tellurique";
-        Mercure.atmosphere= new Atmosphere();
-        Mercure.atmosphere.newAtmoshpere(22,42,7,0,0,0,0,29);
+        Planete mercure= new Planete();
+        mercure.nom = "Mercure";
+        mercure.diametre=4880;
+        mercure.matiere="Tellurique";
+        mercure.atmosphere= new Atmosphere();
+        mercure.atmosphere.newAtmoshpere(22,42,7,0,0,0,0,29);
 
-        Planete Venus= new Planete();
-        Venus.nom = "Venus";
-        Venus.diametre=12100;
-        Venus.matiere="Tellurique";
-        Venus.atmosphere= new Atmosphere();
-        Venus.atmosphere.newAtmoshpere(0,1,0,3,0,0,96,0);
+        Planete venus= new Planete();
+        venus.nom = "Venus";
+        venus.diametre=12100;
+        venus.matiere="Tellurique";
+        venus.atmosphere= new Atmosphere();
+        venus.atmosphere.newAtmoshpere(0,1,0,3,0,0,96,0);
 
-        Planete Terre= new Planete();
-        Terre.nom = "Terre";
-        Terre.diametre=12756;
-        Terre.matiere="Tellurique";
+        Planete terre= new Planete();
+        terre.nom = "Terre";
+        terre.diametre=12756;
+        terre.matiere="Tellurique";
 
-        Planete Mars= new Planete();
-        Mars.nom = "Mars";
-        Mars.diametre=6792;
-        Mars.matiere="Tellurique";
+        Planete mars= new Planete();
+        mars.nom = "Mars";
+        mars.diametre=6792;
+        mars.matiere="Tellurique";
 
-        Planete Jupiter= new Planete();
-        Jupiter.nom = "Jupiter";
-        Jupiter.diametre=142984;
-        Jupiter.matiere="Gazeuse";
+        Planete jupiter= new Planete();
+        jupiter.nom = "Jupiter";
+        jupiter.diametre=142984;
+        jupiter.matiere="Gazeuse";
 
-        Planete Saturne= new Planete();
-        Saturne.nom = "Saturne";
-        Saturne.diametre=120536;
-        Saturne.matiere="Gazeuse";
+        Planete saturne= new Planete();
+        saturne.nom = "Saturne";
+        saturne.diametre=120536;
+        saturne.matiere="Gazeuse";
 
-        Planete Uranus= new Planete();
-        Uranus.nom = "Uranus";
-        Uranus.diametre=51118;
-        Uranus.matiere="Gazeuse";
-        Uranus.atmosphere= new Atmosphere();
-        Uranus.atmosphere.newAtmoshpere(83,0,2.5f,15,0,0,0,0);
+        Planete uranus= new Planete();
+        uranus.nom = "Uranus";
+        uranus.diametre=51118;
+        uranus.matiere="Gazeuse";
+        uranus.atmosphere= new Atmosphere();
+        uranus.atmosphere.newAtmoshpere(83,0,2.5f,15,0,0,0,0);
 
-        Planete Neptune= new Planete();
-        Neptune.nom = "Neptune";
-        Neptune.diametre=49532;
-        Neptune.matiere="Gazeuse";
+        Planete neptune= new Planete();
+        neptune.nom = "Neptune";
+        neptune.diametre=49532;
+        neptune.matiere="Gazeuse";
 
-        Planete Pluton= new Planete();
-        Pluton.nom = "Pluton";
-        Pluton.diametre=2300;
-        Pluton.matiere="Gazeuse";
+        Planete pluton= new Planete();
+        pluton.nom = "Pluton";
+        pluton.diametre=2300;
+        pluton.matiere="Gazeuse";
 
-        System.out.println(Jupiter.nom+" est une planète "+Jupiter.matiere+" avec un diametre de "+Jupiter.diametre+"km.");
+        System.out.println(jupiter.nom+" est une planète "+jupiter.matiere+" avec un diametre de "+jupiter.diametre+"km.");
 
         System.out.println("L'atmosphère de Uranus est composée de :");
-        Uranus.atmosphere.descAtmosphere();
+        uranus.atmosphere.descAtmosphere();
 
     }
 
