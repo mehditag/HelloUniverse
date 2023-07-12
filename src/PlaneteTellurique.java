2ffd60c6423c396fb97562abfb63e890c18db025
mehditag <mehdiitaguema@gmail.com>
2023-07-12 15:38:46 +0200

    Add instanceof keyword

--- src/PlaneteTellurique.java
+++ src/PlaneteTellurique.java
@@ -10,11 +10,16 @@ public class PlaneteTellurique extends Planete implements Habitable{
     public void accueillirVaisseau(Vaisseau nouveauVaisseau){
         totalVisiteurs+=nouveauVaisseau.nbPassagers;
         Vaisseau vaisseauPrecedent=vaisseauAccoste;
+
+        if (nouveauVaisseau instanceof VaisseauDeGuerre){
+            ((VaisseauDeGuerre) nouveauVaisseau).desactiverArmes();
+        }
+
         if (vaisseauPrecedent==null){
             System.out.println("Aucun vaisseau ne s'en va");
         }
         else{
-            System.out.println("Le vaisseau de type "+nouveauVaisseau.type+" doit s'en aller");
+            System.out.println("Le vaisseau de type "+vaisseauPrecedent.type+" doit s'en aller");
         }
         vaisseauAccoste=nouveauVaisseau;
         //return vaisseauPrecedent;
--- src/VaisseauDeGuerre.java
+++ src/VaisseauDeGuerre.java
@@ -1,8 +1,21 @@
 
 public class VaisseauDeGuerre extends Vaisseau {
+    boolean armesDesactivees;
+
     void attaque(Vaisseau vaisseauCible, String arme, int duree){
-        System.out.println("Un vaisseau de type "+type+" attaque un vaisseau de type "+vaisseauCible.type+" en utilisant l'arme "+arme+" pendant "+duree+" minutes.");
-        vaisseauCible.resistanceDuBouclier=0;
-        vaisseauCible.blindage=vaisseauCible.blindage/2;
+
+        if (armesDesactivees){
+            System.out.println("Attaque impossible, l'armement est désactivé");
+        }
+        else {
+            System.out.println("Un vaisseau de type " + type + " attaque un vaisseau de type " + vaisseauCible.type + " en utilisant l'arme " + arme + " pendant " + duree + " minutes.");
+            vaisseauCible.resistanceDuBouclier = 0;
+            vaisseauCible.blindage = vaisseauCible.blindage / 2;
+        }
+    }
+
+    void desactiverArmes(){
+        this.armesDesactivees=true;
+        System.out.println("Désactivation des armes d'un vaisseau de type "+this.type);
     }
 }
